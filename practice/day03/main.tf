c7c2112446a454f736d156e84ca8cc628464bd66
Ebenezer Emmanuel <ebenezer@jamfortetech.com>
2025-11-27 12:26:16 +0100

    solve today's task

--- practice/day03/main.tf
+++ practice/day03/main.tf
@@ -1,22 +1,30 @@
 terraform {
-    required_providers {
-      aws = {
-        source = "hashicorp/aws"
-        version = "~> 6.0"
-      }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
     }
-    
+  }
 }
+
 provider "aws" {
-    region = "eu-west-2"
-    # profile = "friction-eben-cli"
+  region = "eu-west-2"
 }
-resource "aws_s3_bucket" "demo-resource-creation" {
-  bucket = "awsterraformpractice-00998877"
+
+resource "aws_vpc" "demo_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "demo-vpc"
+  }
+}
+
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket        = "awsterraformpractice-00998877"
   force_destroy = true
 
   tags = {
-    Name        = "My bucket 2.0"
+    Name        = "demo-bucket"
     Environment = "Dev"
   }
 }
